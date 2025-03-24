0c7e8c5ec389677f2ec50bfada52a2389b774659
Lucas da Silva <160244553+Lsdceo@users.noreply.github.com>
2025-03-24 15:49:52 -0300

    Delete Ecommerce-Sesi directory

--- Ecommerce-Sesi/src/main/java/com/revisao/ecommerce/dto/PedidoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.revisao.ecommerce.dto;
-
-import java.time.Instant;
-
-import com.revisao.ecommerce.entities.StatusDoPedido;
-
-public record PedidoDTO(Instant momento, StatusDoPedido status) {
-
-}
